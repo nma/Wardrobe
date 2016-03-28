23154f1eadeb48114bcaca4cbaa3abd3a0e28904
nma <nickma38@gmail.com>
2016-03-28 11:42:53 -0400

    swap to a python backend

---
+++ python-flask-server/controllers/default_controller.py
@@ -0,0 +1,84 @@
+
+def create_user(body) -> str:
+    return 'do some magic!'
+
+def create_users_with_array_input(body) -> str:
+    return 'do some magic!'
+
+def create_users_with_list_input(body) -> str:
+    return 'do some magic!'
+
+def login_user(username, password) -> str:
+    return 'do some magic!'
+
+def logout_user() -> str:
+    return 'do some magic!'
+
+def get_user_by_name(username) -> str:
+    return 'do some magic!'
+
+def update_user(username, body) -> str:
+    return 'do some magic!'
+
+def delete_user(username) -> str:
+    return 'do some magic!'
+
+def create_drawer(body) -> str:
+    return 'do some magic!'
+
+def drawer_drawer_id_get(drawerId) -> str:
+    return 'do some magic!'
+
+def drawer_drawer_id_put(drawerId) -> str:
+    return 'do some magic!'
+
+def drawer_drawer_id_delete(drawerId) -> str:
+    return 'do some magic!'
+
+def promote_shelf(wardrobeUrl, promoteUrl) -> str:
+    return 'do some magic!'
+
+def create_promoter(body) -> str:
+    return 'do some magic!'
+
+def promoter_promoter_id_get(promoterId) -> str:
+    return 'do some magic!'
+
+def promoter_promoter_id_put(promoterId) -> str:
+    return 'do some magic!'
+
+def promoter_promoter_id_delete(promoterId) -> str:
+    return 'do some magic!'
+
+def rollback_shelf(wardrobeUrl) -> str:
+    return 'do some magic!'
+
+def add_shelf(body) -> str:
+    return 'do some magic!'
+
+def get_shelf_by_id(shelfId) -> str:
+    return 'do some magic!'
+
+def update_shelf(shelfId) -> str:
+    return 'do some magic!'
+
+def delete_shelf(shelfId) -> str:
+    return 'do some magic!'
+
+def promote_drawer_id(shelfId, promoterId) -> str:
+    return 'do some magic!'
+
+def rollback_to_drawer_id(shelfId, drawerId) -> str:
+    return 'do some magic!'
+
+def create_stack(body) -> str:
+    return 'do some magic!'
+
+def stack_stack_id_get(stackId) -> str:
+    return 'do some magic!'
+
+def stack_stack_id_put(stackId) -> str:
+    return 'do some magic!'
+
+def stack_stack_id_delete(stackId) -> str:
+    return 'do some magic!'
