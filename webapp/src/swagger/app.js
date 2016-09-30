8cc3d43540586c19bb862c8d977e3b47a5a13ba1
nma <nickma38@gmail.com>
2016-09-30 12:07:37 -0400

    update to use swagger-client instead

--- webapp/src/swagger/app.js
+++
@@ -1,9 +0,0 @@
-// initialize swagger client, point to a resource listing
-window.client = new SwaggerClient({
-    url: "http://localhost:8080/api-docs",
-    success: function() {
-        client.Stack.getStack({}, {responseContentType: 'application/json'}, function(data) {
-            document.getElementById("mydata").innerHTML = JSON.stringify(data.obj);
-        });
-    }
-});
